c5ba7b2cb6b82120e9e0df1251d25984d70e8ba6
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-29 23:53:24 +0530

    ddd

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,48 +1,4 @@
-// // Step.js
-// import React from 'react';
 
-// const Step = ({ step, formData, handleChange, nextStep, prevStep, handleSubmit }) => {
-//   const renderStep = () => {
-//     switch (step) {
-//       case 1:
-//         return (
-//           <div id="step1">
-//             <input type="text" id="first_name" value={formData.first_name} onChange={handleChange} placeholder="First Name" />
-//             <br />
-//             <input type="text" id="last_name" value={formData.last_name} onChange={handleChange} placeholder="Last Name" />
-//           </div>
-//         );
-//       case 2:
-//         return (
-//           <div id="step2">
-//             <input type="text" id="model" value={formData.model} onChange={handleChange} placeholder="Car Model" />
-//             <br />
-//             <input type="number" id="car_price" value={formData.car_price} onChange={handleChange} placeholder="Car Price" />
-//           </div>
-//         );
-//       case 3:
-//         return (
-//           <div id="step3">
-//             <input type="text" id="card_info" value={formData.card_info} onChange={handleChange} placeholder="Card Information" />
-//             <br />
-//             <input type="text" id="expiry_date" value={formData.expiry_date} onChange={handleChange} placeholder="Expiry Date" />
-//           </div>
-//         );
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {renderStep()}
-//       {step !== 1 && <button type="button" id="prevBtn" onClick={() => prevStep().then()}>Previous</button>}
-//       {step !== 3 ? <button type="button" id="nextBtn" onClick={() => nextStep().then()}>Next</button> : <button type="button" id="submitBtn" onClick={handleSubmit}>Submit</button>}
-//     </div>
-//   );
-// };
-
-// export default Step;
 
 import React from 'react';
 
